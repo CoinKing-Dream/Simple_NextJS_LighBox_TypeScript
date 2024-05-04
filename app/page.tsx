862bfe1d0915de29ccbffbc6d40ed59db85f423c
MitsuiMinoru <mitsuimino711@gamil.com>
2024-05-04 16:52:06 +1000

    Update Typescirpt...

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,72 @@
+'use client';
+
 import Image from "next/image";
-import styles from "./page.module.css";
+import Link from "next/link";
+import $ from "jquery";
+import { useEffect, useState } from "react";
+import lightbox from 'lightbox2';
+import 'lightbox2/dist/css/lightbox.css';
+
+const numbers: number[] = Array.from({length:30}, (v, i) => i + 1);
+const image_link: string = "https://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/6-2-1/img/";
 
 export default function Home() {
+
+  useEffect(() => {
+    if (typeof window!== 'undefined') {
+      lightbox.init();
+    }
+
+    const fadeAnime = () => {
+      $('.gallery li').each(function() {
+        var elemPos = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll >= elemPos - windowHeight) {
+          $(this).addClass('flipLeft');
+        } else {
+          $(this).removeClass('flipLeft');
+        }
+      });
+    };
+
+    fadeAnime();
+
+    $(window).scroll(fadeAnime);
+
+    return () => {
+      $(window).off('scroll', fadeAnime);
+    };
+  }, []);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div>
+      <lightboxStyles />
+      <header className="header">
+        <h1>sample</h1>
+      </header>
+      <div className="wrapper">
+        <h2>複数画像を並列に見せる</h2>
+        <ul className="gallery" >
+          {
+            numbers.map((number) => {
+              return (
+                <li>
+                  <Link href={image_link + ((number < 10) ? ("0" + number) : number) + ".jpg"}
+                    data-lightbox={((number < 17) ? "gallery1" : (number < 25) ? "gallery2" : "gallery3")}
+                    data-title="グループ1キャプション">
+                    <Image 
+                      src={image_link + ((number < 10) ? ("0" + number) : number) + ".jpg"}
+                      width={1000}
+                      height={1000}
+                      alt="" />                    
+                  </Link>
+                </li>
+              )
+            })
+          }
+        </ul>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
